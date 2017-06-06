1f107a3d6dd343aaaf9d5ad5eeee2201cb105e7f
kiruthikcs <kiruthika.a.k@capgemini.com>
2017-06-06 09:45:04 +0530

    Add files via upload

---
+++ SimpleInheritancePrgm/src/com/cg/pack/TestInheritance.java
@@ -0,0 +1,12 @@
+package com.cg.pack;
+
+public class TestInheritance {
+
+	 public static void main(String args[]) 
+	    {
+	         
+	        MountainBike mb = new MountainBike(3, 100, 25);
+	        System.out.println(mb.toString());
+	             
+	    }
+}
